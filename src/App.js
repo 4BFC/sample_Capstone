81de735f6c53d0f457ebf6d4196517593f1d6b97
4BFC <4bee.code@gmail.com>
2024-02-25 12:43:09 +0900

    feat: apply markdown and implement exam sorting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import SelectExam from "./components/SelectExam";
 import LabExam from "./components/LabExam";
-import MD_test from "./components/Markdown_test"
+import MD_test from "./test/markdown/Markdown_test"
+import MD_css from "./test/markdown/MD_css"
+import Test from "./test/markdown/test"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<SelectExam />} />
           <Route path="/lab" element={<LabExam />} />
           <Route path="/MD_test" element={<MD_test />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/MD_css" element={<MD_css />} />
         </Routes>
       </div>
     </BrowserRouter>
