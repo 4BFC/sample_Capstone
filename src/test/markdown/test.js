c43d6ddd4585794201f498d3ef1e73932b1855e3
4BFC <4bee.code@gmail.com>
2024-02-28 23:02:49 +0900

    feat: edited driver_test json file and insert HTML img tag to questions array

--- src/test/markdown/test.js
+++ src/test/markdown/test.js
@@ -1,39 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function Markdown_test() {
+export default function LabExam() {
   const [data, setData] = useState([]);
+  const [limit, setLimit] = useState(5);
 
   useEffect(() => {
     fetch('http://localhost:3001/sample')
       .then(res => res.json())
       .then((data) => {
-        setData(data.slice(0, 5)); // limit에 맞게 잘라서 사용
+        setData(data.slice(0, limit)); // limit에 맞게 잘라서 사용
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
+  }, [limit]);
 
   return (
     <div>
+      <h2>Test</h2>
+      <select onChange={(e) => { setLimit(e.target.value); }}>
+        <option value={5}>5</option>
+        <option value={10}>10</option>
+        <option value={15}>15</option>
+        <option value={20}>20</option>
+      </select>
       <Link to="/">back</Link>
-      <div style={{ display: 'absolute', margin: '10px', height: '100px', width: '400px' }}>
-        <img src="https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004" style={{ float: 'left', width: '200px', margin: '10px', marginBottom: '0px' }} />Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti perspiciatis quo quia ea vitae iusto excepturi autem quod molestias beatae eveniet fugiat corporis delectus, eos fugit iure cupiditate optio eaque?Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti <img src="https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004" style={{ width: '20px', margin: '0px', marginBottom: '0px' }} />perspiciatis quo quia ea vitae iusto excepturi autem quod molestias beatae eveniet fugiat corporis delectus, eos fugit iure cupiditate optio eaque?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est harum rerum repudiandae aliquid eaque nihil delectus, saepe facere aut, ab qui. Temporibus, reiciendis? Atque quasi facere expedita reprehenderit modi voluptatem!
-        {/* <ReactMarkdown>{markdown}</ReactMarkdown> */}
-        <div style={{ display: 'absolute' }}>
-          <ol>
-            {data.map((item) => (
-              <li key={item.id}><p>{item.question}</p>{item.options}</li>
-              // <li key={item.id}>
-              //   <ReactMarkdown>
-              //     {`### ${item.question}\n\n${item.options}`}
-              //   </ReactMarkdown>
-              // </li>
+      <ol>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h1>test</h1>
+            {/* 이미지 */}
+            <img src="https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg" className="test_image" style={{ width: "250px" }} />
+            {/* 질문 */}
+            <p>{item.question}</p>
+            {/* 4선지 */}
+            {item.options.map((option, index) => (
+              <div key={index}>{option}</div>
             ))}
-          </ol>
-        </div>
-      </div>
-    </div >
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
