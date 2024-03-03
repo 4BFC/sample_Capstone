64090134b1b04b953a660d29b3be1d45123c10a2
4BFC <4bee.code@gmail.com>
2024-03-03 19:55:44 +0900

    feat: edited driver_test json file applied exam multi image rendering implement

--- src/test/html_parse_img/test.js
+++ src/test/html_parse_img/test.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import parse from 'html-react-parser'; // html-react-parser 모듈 사용
+import parse from 'html-react-parser';
 
 export default function LabExam() {
   const [data, setData] = useState([]);
@@ -10,25 +10,40 @@ export default function LabExam() {
     fetch('http://localhost:3001/sample')
       .then(res => res.json())
       .then((data) => {
-        setData(data.slice(0, limit)); // limit에 맞게 잘라서 사용
+        setData(data.slice(0, limit));
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, [limit]);
 
-  //HTML 문자열에 포함된 <img> 태그를 실제 이미지로 변경하는 함수
-  const parseImageTag = (question, question_img_url, question_img_class) => {
-    // 정규식을 사용하여 <img src=1> 문자열을 찾는다.
-    const imgRegex = /<img.*?\/?>/g;
-    // 대체할 이미지 태그로 교체한다.
-    return question.replace(imgRegex, `<img src="${question_img_url}" className="${question_img_class}" />`);
+  const parseImageTag = (question, questionImgUrls) => {
+    let parsedQuestion = question;
+
+    // 이미지 URL 배열이 정의되었는지 확인합니다.
+    if (Array.isArray(questionImgUrls) && questionImgUrls.length > 0) {
+      // 이미지 URL 배열의 각 요소를 반복하여 이미지 태그를 생성하고 질문에 삽입합니다.
+      questionImgUrls.forEach((imageUrl, index) => {
+        // 이미지 태그를 생성합니다. index + 1을 사용하여 이미지 태그의 src 속성에 대응하는 번호를 삽입합니다.
+        const imgTag = `<img src="${imageUrl}"/>`;
+
+        // 이미지 태그를 질문에 삽입합니다.
+        parsedQuestion = parsedQuestion.replace(`<img src=${index + 1}/>`, imgTag);
+      });
+    }
+    else {
+      const imgRegex = /<img.*?\/?>/g;
+      // 이미지 URL 배열이 없는 경우 직접적으로 이미지 태그를 대체합니다.
+      parsedQuestion = parsedQuestion.replace(imgRegex, `<img src="${questionImgUrls}"/>`);
+    }
+
+    return parsedQuestion;
   };
 
   return (
     <div>
       <h2>Test</h2>
-      <select onChange={(e) => { setLimit(e.target.value); }}>
+      <select onChange={(e) => { setLimit(parseInt(e.target.value)); }}>
         <option value={5}>5</option>
         <option value={10}>10</option>
         <option value={15}>15</option>
@@ -39,7 +54,7 @@ export default function LabExam() {
         {data.map((item, index) => (
           <li key={index}>
             {/* 질문 */}
-            <p>{parse(parseImageTag(item.question, item.image, "test_image"))}</p>
+            <p>{parse(parseImageTag(item.question, item.images))}</p>
             {/* 4선지 */}
             {item.options.map((option, index) => (
               <div key={index}>{option}</div>
