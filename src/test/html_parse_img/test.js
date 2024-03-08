23e6844b6e86af06ab40d84dc7106dcb10a70525
4BFC <4bee.code@gmail.com>
2024-03-08 12:11:37 +0900

    feat: edited driver_test json file and change an existing function

--- src/test/html_parse_img/test.js
+++ src/test/html_parse_img/test.js
@@ -20,6 +20,7 @@ export default function LabExam() {
   }, [limit]);
 
   //정규식으로 파싱 후에 이미지 렌더링 함수
+  /**
   const parseImageTag = (question, questionImgUrls) => {
     let parsedQuestion = question;
 
@@ -41,21 +42,92 @@ export default function LabExam() {
     // }
 
     return parsedQuestion;
+  }; 
+  */
+  // const renderQuestionWithImages = (question, questionImages) => {
+  //   // 유효한 이미지 배열인지 확인합니다.
+  //   if (!Array.isArray(questionImages)) {
+  //     return <div>No images available</div>;
+  //   }
+
+  //   // 정규 표현식을 사용하여 이미지 태그를 파싱합니다.
+  //   const regex = /<img src=(\d+)>/g;
+
+  //   // question을 정규 표현식으로 파싱하여 이미지 태그를 찾고, 각 이미지 태그를 적절한 URL로 교체합니다.
+  //   const parsedQuestion = question.replace(regex, (match, imgUrlIndex) => {
+  //     imgUrlIndex = parseInt(imgUrlIndex); // 이미지 URL 인덱스
+
+  //     // 인덱스가 유효한지 확인합니다.
+  //     if (!isNaN(imgUrlIndex) && imgUrlIndex >= 0 && imgUrlIndex < questionImages.length) {
+  //       const imageUrl = questionImages[imgUrlIndex].url; // 이미지 URL을 가져옵니다.
+  //       return `<img src="${imageUrl}" alt="Image ${imgUrlIndex}" />`;
+  //     } else {
+  //       return match; // 유효하지 않은 인덱스인 경우 원래의 이미지 태그를 반환합니다.
+  //     }
+  //   });
+
+  //   // 파싱된 질문을 반환합니다.
+  //   return <div dangerouslySetInnerHTML={{ __html: parsedQuestion }} />;
+  // };
+
+  const renderQuestionWithImages = (question, questionImages) => {
+    // 유효한 이미지 배열인지 확인합니다.
+    if (!Array.isArray(questionImages)) {
+      return <div>No images available</div>;
+    }
+
+    // 정규 표현식을 사용하여 이미지 태그를 파싱합니다.
+    const regex = /<img src=(\d+)>/g;
+
+    // question을 정규 표현식으로 파싱하여 이미지 태그를 찾고, 각 이미지 태그를 적절한 URL로 교체합니다.
+    const parsedQuestion = question.replace(regex, (match, imgUrlIndex) => {
+      imgUrlIndex = parseInt(imgUrlIndex); // 이미지 URL 인덱스
+
+      // 인덱스가 유효한지 확인합니다.
+      if (!isNaN(imgUrlIndex) && imgUrlIndex >= 0 && imgUrlIndex < questionImages.length) {
+        const imageUrl = questionImages[imgUrlIndex].url; // 이미지 URL을 가져옵니다.
+        return `<img src="${imageUrl}" alt="Image ${imgUrlIndex}" />`;
+      } else {
+        return match; // 유효하지 않은 인덱스인 경우 원래의 이미지 태그를 반환합니다.
+      }
+    });
+
+    // 파싱된 질문을 반환합니다.
+    return <div>{parse(parsedQuestion)}</div>;
   };
 
-  // 이미지 렌더링 함수
-  const renderImages = (outsideImage) => {
-    if (Array.isArray(outsideImage) && outsideImage.length > 0) {
-      return outsideImage.map((image, imageIndex) => (
-        <img key={imageIndex} src={image} alt={`Image ${imageIndex + 1}`} style={{ width: '100%' }} />
-      ));
-    } else if (typeof outsideImage === 'string' && outsideImage.trim() === '') {
-      // outside_image 필드가 빈 문자열인 경우
-      return null; // 빈 문자열일 경우 아무것도 반환하지 않습니다.
+  // 이미지 렌더링 함수 수정
+  const renderImages = (imageData) => {
+    if (Array.isArray(imageData) && imageData.length > 0) {
+      return (
+        <div>
+          {imageData.map((image, imageIndex) => (
+            <img
+              key={imageIndex}
+              src={image.url} // 이미지 객체에서 URL을 가져옵니다.
+              alt={`Image ${imageIndex + 1}`}
+              style={{ width: '100%' }}
+            />
+          ))}
+        </div>
+      );
     } else {
-      return <img src={outsideImage} alt="Image" style={{ width: '100%' }} />;
+      return null;
     }
   };
+
+  // const renderImages = (outsideImage) => {
+  //   if (Array.isArray(outsideImage) && outsideImage.length > 0) {
+  //     return outsideImage.map((image, imageIndex) => (
+  //       <img key={imageIndex} src={image} alt={`Image ${imageIndex + 1}`} style={{ width: '100%' }} />
+  //     ));
+  //   } else if (typeof outsideImage === 'string' && outsideImage.trim() === '') {
+  //     // outside_image 필드가 빈 문자열인 경우
+  //     return null; // 빈 문자열일 경우 아무것도 반환하지 않습니다.
+  //   } else {
+  //     return <img src={outsideImage} alt="Image" style={{ width: '100%' }} />;
+  //   }
+  // };
 
   //rendering
   return (
@@ -72,18 +144,25 @@ export default function LabExam() {
         {data.map((item, index) => (
           <li key={index} style={{ marginBottom: '70px', border: '1px solid black', width: '60%' }}>
             {/* 질문 */}
-            <p>{parse(parseImageTag(item.question, item.images))}</p>
-            {/* 이미지 렌더링 함수 호출 */}
-            {renderImages(item.outside_image)}
+            {/* <p>{parseImageTag(item.question, item.question_images_in)}</p> */}
+            {renderQuestionWithImages(item.question, item.question_images_in)}
+            {/* 이미지 렌더링 */}
+            <div>{renderImages(item.question_images_out)}</div>
             {/* 4선지 */}
-            {item.options.map((option, optionIndex) => (
-              <div key={optionIndex}>
-                {option}
-              </div>
-            ))}
+            <ol>
+              {item.options.map((option, optionIndex) => (
+                <li key={optionIndex}>{option}</li>
+              ))}
+            </ol>
           </li>
         ))}
       </ol>
+      <div style={{ width: "1000px" }}>
+        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fnspuc%2FbtsFt5GRCru%2FQyCoV4BBeEMoBGyNWoMCRk%2Fimg.png"></img>
+        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fnspuc%2FbtsFt5GRCru%2FQyCoV4BBeEMoBGyNWoMCRk%2Fimg.png"></img>
+        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fnspuc%2FbtsFt5GRCru%2FQyCoV4BBeEMoBGyNWoMCRk%2Fimg.png"></img>
+        <img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fnspuc%2FbtsFt5GRCru%2FQyCoV4BBeEMoBGyNWoMCRk%2Fimg.png"></img>
+      </div>
 
     </div>
   );
