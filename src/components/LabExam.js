19bffed68b47bf61a51a2ea3b9d262f955ff8aaf
4BFC <4bee.code@gmail.com>
2024-03-11 14:13:01 +0900

    feat: test file merge to labExam file

--- src/components/LabExam.js
+++ src/components/LabExam.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import parse from 'html-react-parser';
+import "../css/style.css"
 // shuffleArray(Fisher-Yates) 알고리즘을 사용하여 배열을 섞는 함수
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -18,10 +19,7 @@ export default function LabExam() {
     fetch('http://localhost:3001/sample')
       .then(res => res.json())
       .then((data) => {
-        setData(data.slice(0, limit)); // slice를 통해서 10개의 아이템만 가져올 것 -> useEffect의 특징상 한번 호출하고 다시 re-render가 되지 않는다. 따라서 useEffect를 limit에 의존 시켜야한다.
-        // map((data)=>{cons})
-        console.log(data); //data 배열 값 확인
-        console.log(data[0]); //data 배열 값 확인
+        setData(data.slice(0, limit));
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -32,6 +30,41 @@ export default function LabExam() {
   const handleShuffle = () => {
     const shuffledData = shuffleArray(data);
     setData([...shuffledData]); // 새로운 배열로 설정하여 React가 업데이트를 감지할 수 있게 함
+  };
+
+
+  //클래스 추가
+  // inside 이미지 파싱 후 렌더링 함수 수정
+  const parseImageTag = (question, questionImgUrls) => {
+    let parsedQuestion = question;
+
+    // 이미지 URL 배열이 정의되었는지 확인합니다.
+    if (Array.isArray(questionImgUrls) && questionImgUrls.length > 0) {
+      // 이미지 URL 배열의 각 요소를 반복하여 이미지 태그를 생성하고 질문에 삽입합니다.
+      questionImgUrls.forEach((imageUrl) => {
+        // 정규 표현식 리터럴을 사용하여 이미지 태그를 대체합니다.
+        const regex = /src=\d+/g;
+        parsedQuestion = parsedQuestion.replace(regex, `src="${imageUrl.url}" class="${imageUrl.attribute}"`);
+      });
+    }
+    return parse(parsedQuestion);
+  };
+
+  // outside 이미지 렌더링 함수 수정
+  const renderImages = (imageData) => {
+    if (Array.isArray(imageData) && imageData.length > 0) {
+      return imageData.map((image, imageIndex) => (
+        <img
+          key={imageIndex}
+          src={image.url} // 이미지 객체에서 URL을 가져옵니다.
+          class={image.attribute}
+          alt={`Image ${imageIndex}`}
+        // style={{ width: '100%' }}
+        />
+      ));
+    } else {
+      return null;
+    }
   };
 
   return (
@@ -46,9 +79,21 @@ export default function LabExam() {
         <option value={20}>20</option>
       </select>
       <Link to="/">back</Link>
-      <ol>
-        {data.map((data) => (
-          <li key={data.id}><p>{data.question}</p>{data.options}</li>
+      <ol style={{ listStylePosition: 'inside' }}>
+        {/* , listStyleType: 'none'   */}
+        {data.map((item, index) => (
+          <li key={index} style={{ marginBottom: '70px', border: '1px solid black', width: '70%' }}>
+            {/* 질문 */}
+            <p>{parseImageTag(item.question, item.question_images_in)}</p>
+            {/* 이미지 렌더링 */}
+            <div style={{ width: '500px' }}>{renderImages(item.question_images_out)}</div>
+            {/* 4선지 */}
+            <ol style={{ listStyleType: 'none' }}>
+              {item.options.map((option, optionIndex) => (
+                <li key={optionIndex}>{option}</li>
+              ))}
+            </ol>
+          </li>
         ))}
       </ol>
     </div >
